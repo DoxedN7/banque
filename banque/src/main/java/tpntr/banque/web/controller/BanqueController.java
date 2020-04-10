a94b8e8dd62671bc4e9db8c3c88ff840c0e19645
arthurquique <arthurquique@protonmail.com>
2020-04-10 18:18:59 +0200

    Commit 1

---
+++ banque/src/main/java/tpntr/banque/web/controller/BanqueController.java
@@ -0,0 +1,13 @@
+package tpntr.banque.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BanqueController {
+	@RequestMapping(value="/Produits", method=RequestMethod.GET)
+    public String listeProduits() {
+        return "Un exemple de produit";
+    }
+}
