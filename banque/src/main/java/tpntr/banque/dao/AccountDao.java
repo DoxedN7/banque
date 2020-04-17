8d21c30fe04c861c49de796efc18db6bedb23061
arthurquique <arthurquique@protonmail.com>
2020-04-17 13:49:43 +0200

    )

--- banque/src/main/java/tpntr/banque/dao/AccountDao.java
+++ banque/src/main/java/tpntr/banque/dao/AccountDao.java
@@ -1,15 +1,14 @@
 package tpntr.banque.dao;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import tpntr.banque.model.*;
 
-import tpntr.banque.Account;
+import java.util.List;
 
 @Repository
-public interface AccountDao extends JpaRepository<Account, Integer>{
-	 public List<Account>findAll();
-	 public Account findById(int id);
-	 public Account save(Account account);
+public interface AccountDao extends JpaRepository<Account, Integer> {
+
 }
--- banque/src/main/java/tpntr/banque/web/controller/BanqueController.java
+++ banque/src/main/java/tpntr/banque/web/controller/BanqueController.java
@@ -1,34 +1,69 @@
 package tpntr.banque.web.controller;
 
 import java.util.List;
+import tpntr.banque.model.*;
+import tpntr.banque.web.exceptions.CompteIntrouvableException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import tpntr.banque.Account;
-import tpntr.banque.dao.AccountDao;
 
-@RestController
+@Controller
 public class BanqueController {
-	
-	@Autowired
-	private AccountDao accountDao;
-	
-    //Récupérer la liste des produits
-	@RequestMapping(value="/Comptes", method=RequestMethod.GET)
-    public List<Account> accountList() {
-        return accountDao.findAll();
+    private static List<Account> accounts = new ArrayList<Account>();
+    
+    static {
+        accounts.add(new Account(1, "Younes", "Djelmoudi", 999999));
+        accounts.add(new Account(2, "Youes", "Djelmoudi", 999999));
+        accounts.add(new Account(3, "Yunes", "Djelmoudi", 999999));
+        accounts.add(new Account(4, "ounes", "Djelmoudi", 999999));
+        accounts.add(new Account(5, "Youne", "Djelmoudi", 999999));
+        
     }
+ 
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+ 
+    	
+        String message = "Hello Spring Boot + JSP";
 
-	//Afficher un compte
-	@GetMapping(value = "/Comptes/{id}")
-	public Account displayAccount(@PathVariable int id) {
-		return accountDao.findById(id);
-	}
+    	//On enregistre le message dans le modèle
+        model.addAttribute("message", message);
+ 
+        return "index";
+    }
+ 
+    @GetMapping(value="/accountInfo/{id}")
+    public String viewAccountInfo(Model model, @PathVariable int id) {
+    	Account accountToDisplay = getAccountWithId(id);
+    	if(accountToDisplay != null) {
+    		model.addAttribute("account",accountToDisplay);
+    	}
+    	else throw new CompteIntrouvableException("Le compte avec l'id " + id + " n'existe pas.");
+        return "accountInfo";
+    }	
+    
+    public Account getAccountWithId(int id) {
+    	Account accountToReturn = null;
+    	for(int i = 0; i<accounts.size();i++) {
+    		if(accounts.get(i).getId() == id) {
+    			accountToReturn = accounts.get(i);
+    		}
+    	}
+    	return accountToReturn;
+    }
+    
+    @RequestMapping(value = { "/accountList" }, method = RequestMethod.GET)
+    public String viewAccountList(Model model) {
+ 
+        model.addAttribute("accounts", accounts);
+ 
+        return "accountList";
+    }	
 	
-	//Ajouter un compte
-	@PostMapping(value = "/Comptes")
-	public void addAccount(@RequestBody Account account) {
-		accountDao.save(account);
-	}
 }
